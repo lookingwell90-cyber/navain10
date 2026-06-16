b56eda454fb689903bdd5b10aa538ee55df831ae
lookingwell90-cyber <lookingwell90@gmail.com>
2026-06-16 22:04:27 +0500

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDteptTOhJ1gjmNOrrnrIfepe2neg6hmkU",
+  authDomain: "my-leads-app-f6198.firebaseapp.com",
+  projectId: "my-leads-app-f6198",
+  storageBucket: "my-leads-app-f6198.firebasestorage.app",
+  messagingSenderId: "239949793404",
+  appId: "1:239949793404:web:092e751967d04be2c8e1f9"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+export const authReady = signInAnonymously(auth).catch(err => {
+  console.error("Firebase auth error:", err);
+});
